9423e471f4fd783e0bfa199bf39ef53fe4064d00
Joao Pedro <pedro.arofilgueira@gmail.com>
2026-03-24 10:39:53 -0300

    add

---
+++ src/main/java/com/estudo/projetoSpringBoot/Resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.estudo.projetoSpringBoot.Resources;
+
+import com.estudo.projetoSpringBoot.Entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> finAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
