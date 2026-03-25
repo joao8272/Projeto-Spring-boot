a2f6e1d5886d44381491553915f8f7c4dbb16b30
Joao Pedro <pedro.arofilgueira@gmail.com>
2026-03-25 13:34:41 -0300

    add

--- src/main/java/com/estudo/projetoSpringBoot/Repositories/UserRepository.java
+++ src/main/java/com/estudo/projetoSpringBoot/Repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.estudo.projetoSpringBoot.Repositories;
 import com.estudo.projetoSpringBoot.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
 }
---
+++ src/main/java/com/estudo/projetoSpringBoot/Services/UserService.java
@@ -0,0 +1,24 @@
+package com.estudo.projetoSpringBoot.Services;
+
+import com.estudo.projetoSpringBoot.Entities.User;
+import com.estudo.projetoSpringBoot.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
